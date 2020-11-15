436f1da67eff2cf73db123fadba7123f4d85b11e
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-11-15 08:58:29 -0600

    set up stripe instance and create new charge route

--- src/routes/__test__/new.test.ts
+++ src/routes/__test__/new.test.ts
@@ -2,6 +2,7 @@ import { OrderStatus } from '@nielsendigital/ms-common';
 import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import { stripe } from '../../stripe';
 
 it('should return 404 when order does not exist for purchase', async () => {
   await request(app)
@@ -54,4 +55,41 @@ it('should return 400 when order has been canceled', async () => {
       orderId: order.id,
     })
     .expect(400);
+});
+
+it('should return a 201 with valid inputs', async () => {
+  const userId = global.generatedUserId;
+
+  const price = Math.floor(Math.random() * 100000);
+
+  const order = Order.build({
+    id: global.generatedOrderId,
+    userId,
+    version: 0,
+    price,
+    status: OrderStatus.Created,
+  });
+
+  await order.save();
+
+  const response = await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.getAuthCookie(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+      description: 'Test Suite Transaction from new.test.ts',
+    })
+    .expect(201);
+
+  const stripeCharges = await stripe.charges.list({ limit: 50 });
+
+  const stripeCharge = stripeCharges.data.find((charge) => {
+    return charge.amount === price * 100;
+  });
+
+  expect(stripeCharge).toBeDefined();
+
+  expect(stripeCharge!.currency).toEqual('usd');
+  expect(stripeCharge!.amount).toEqual(price * 100);
 });
--- src/routes/new.ts
+++ src/routes/new.ts
@@ -7,8 +7,11 @@ import {
   NotAuthorizedError,
   NotFoundError,
   OrderStatus,
+  logIt,
+  LogType,
 } from '@nielsendigital/ms-common';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 // instantiate router
 const router = express.Router();
@@ -26,8 +29,17 @@ router.post(
   async (req: Request, res: Response) => {
     const { token, orderId } = req.body;
 
+    // Description is used to describe to the purchaser in their credit card
+    // statement what the Stripe charge is for.
+    let description = req.body.description;
+
+    if (!description) {
+      description = 'Ticket Purchase';
+    }
+
     const order = await Order.findById(orderId);
 
+    // Require that an order be found
     if (!order) {
       throw new NotFoundError('An order was not found for payment to be applied.');
     }
@@ -38,6 +50,10 @@ router.post(
       throw new NotAuthorizedError('You are not authorized to make payment for this order.');
     }
 
+    // Require that the order *NOT* be canceled
+    // Currently CancledGeneric is the only status in use. Eventually, we'll
+    // iterate to handling different kinds of cancelations to make
+    // better business analytics.
     if (
       order.status === OrderStatus.CanceledGeneric ||
       order.status === OrderStatus.CanceledByUser ||
@@ -47,7 +63,18 @@ router.post(
       throw new BadRequestError('Order has been canceled, payment cannot be accepted.');
     }
 
-    res.send({ success: true });
+    // create Stripe Charge
+    await stripe.charges.create({
+      currency: 'usd',
+      // convert dollars into cents for stripe
+      amount: order.price * 100,
+      description,
+      source: token,
+    });
+
+    logIt.out(LogType.SENT, `Charge for order (${order.id} for $ ${order.price}) with token ${token}`);
+
+    res.status(201).send({ success: true, orderId: order.id, price: order.price, token: token });
   }
 );
 
